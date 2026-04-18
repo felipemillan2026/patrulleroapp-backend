eede99131dfce4d7522275ced6bc6a88ec303026
felipemillan2026 <felipe.millan@alumno.iplacex.cl>
2026-04-17 22:47:01 -0400

    Ajusted de seguridad

--- src/main/java/cl/patrulleroapp/backend/security/SecurityConfig.java
+++ src/main/java/cl/patrulleroapp/backend/security/SecurityConfig.java
@@ -47,12 +47,15 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        config.setAllowedHeaders(List.of("*"));
-        config.setAllowCredentials(true);
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-        return source;
+        config.setAllowedOrigins(List.of(
+            "http://localhost:5173",
+            "https://endearing-lokum-fdbb56.netlify.app"
+       ));
+       config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+       config.setAllowedHeaders(List.of("*"));
+       config.setAllowCredentials(true);
+       UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+       source.registerCorsConfiguration("/**", config);
+       return source;
     }
 }
